4357fc0210095c97641ca30bf217763537bf45ba
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2024-11-13 17:25:44 +0900

    11월 13일 와일드 카드 복습

---
+++ datastructure/generic/ex5/WildcardMain1.java
@@ -0,0 +1,27 @@
+package datastructure.generic.ex5;
+
+import datastructure.generic.animal.Animal;
+import datastructure.generic.animal.Cat;
+import datastructure.generic.animal.Dog;
+
+public class WildcardMain1 {
+
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Dog> dogBox = new Box<Dog>();
+        Box<Cat> catBox = new Box<Cat>();
+
+        dogBox.set(new Dog("멍멍이",100));
+
+        WildcardEx.<Dog>printGenericV1(dogBox);
+        WildcardEx.printWildCardV1(dogBox);
+
+        WildcardEx.printGenericV2(dogBox);
+        WildcardEx.printWildCardV2(dogBox);
+
+        Dog dog = WildcardEx.printAndReturnGeneric(dogBox);
+        Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
+
+    }
+
+}
