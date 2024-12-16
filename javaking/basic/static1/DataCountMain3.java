883e5b1a04b687c81d4a2cf6b8a32b014327eea5
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2024-12-17 00:28:56 +0900

    스태틱 변수 학습

--- javaking/basic/static1/DataCountMain3.java
+++ javaking/basic/static1/DataCountMain3.java
@@ -4,13 +4,21 @@ public class DataCountMain3 {
     public static void main(String[] args) {
         Counter counter = new Counter();
         Data3 data1 = new Data3("A");
-        System.out.println("A count=" + data1.count);
+        System.out.println("A count=" + Data3.count);
 
         Data3 data2 = new Data3("B");
-        System.out.println("B count=" + data2.count);
+        System.out.println("B count=" + Data3.count);
 
         Data3 data3 = new Data3("C");
-        System.out.println("C count=" + data3.count);
+        System.out.println("C count=" + Data3.count);
+
+        //추가
+        //인스턴스를 통한 접근
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
         
         System.out.println(data1);
         System.out.println(data2);
