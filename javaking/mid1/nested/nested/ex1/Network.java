71d67e1380813b0340d23cbb01cc99cb8939b22c
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-01-03 00:33:14 +0900

    내부 클래스 / 중첩 클래스 학습

---
+++ javaking/mid1/nested/nested/ex1/Network.java
@@ -0,0 +1,10 @@
+package javaking.mid1.nested.nested.ex1;
+
+public class Network {
+
+    public void sendMessage(String text){
+        NetworkMessage networkMessage = new NetworkMessage(text);
+        networkMessage.print();
+    }
+
+}
---
+++ javaking/mid1/nested/nested/ex1/NetworkMessage.java
@@ -0,0 +1,14 @@
+package javaking.mid1.nested.nested.ex1;
+
+public class NetworkMessage {
+
+    private String content;
+
+    public NetworkMessage(String content) {
+        this.content = content;
+    }
+
+    public void print(){
+        System.out.println("content = " + content);
+    }
+}
