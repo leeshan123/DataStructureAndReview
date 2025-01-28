ad8d780d7038058d7396f41672e9d63d79fa3b9b
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-01-29 01:46:23 +0900

    리스트 자료구조 공부1

---
+++ javaking/mid2/collection/array/MyArrayListV3Main.java
@@ -0,0 +1,34 @@
+package javaking.mid2.collection.array;
+
+public class MyArrayListV3Main {
+    public static void main(String[] args) {
+        MyArrayListV3 list = new MyArrayListV3();
+
+        //마지막에 추가 // O(1)
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+
+        //원하는 위치에 추가
+        System.out.println("addLast");
+        list.add(3,"addList");
+        System.out.println(list);
+
+        System.out.println("addFirst");
+        list.add(0,"addFirst");
+        System.out.println(list);
+
+        //삭제
+        Object remove1 = list.remove(4);
+        System.out.println("removed(4) = " + remove1);
+        System.out.println(list);
+
+        Object remove2 = list.remove(0);
+        System.out.println("remove(0)= " + remove2);
+        System.out.println(list);
+
+    }
+
+
+}
