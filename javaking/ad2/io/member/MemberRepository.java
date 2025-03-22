b2fd8acdcdf1b2e349ea7ad8cd7fa905f7094eb6
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-03-22 16:40:42 +0900

    자바 I/0 마무리

---
+++ javaking/ad2/io/member/MemberRepository.java
@@ -0,0 +1,12 @@
+package javaking.ad2.io.member;
+
+import java.util.List;
+
+public interface MemberRepository {
+
+    void add(Member member);
+
+    List<Member> findAll();
+
+
+}
---
+++ javaking/ad2/io/text/TextConst.java
@@ -0,0 +1,7 @@
+package javaking.ad2.io.text;
+
+public class TextConst {
+    public static final String FILE_NAME = "temp/hello.txt";
+
+
+}
