595ec14eaf949cd1a71aa70add6bbe5121b95763
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-05-19 12:54:44 +0900

    Title: 세 정수형 변수 선언, Time: 99ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250519/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int x = 7;
+        int y = 23;
+        
+                System.out.println(x+" + "+y+" = "+ (x+y));
+
+
+    }
+}
