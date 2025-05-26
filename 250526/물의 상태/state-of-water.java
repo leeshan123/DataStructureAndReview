fa2f985d3aee0bfb1cde9296265e7715d6950ecf
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-05-26 12:29:14 +0900

    Title: 물의 상태, Time: 93ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250526/물의 상태/state-of-water.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int temp = Integer.parseInt(br.readLine());
+
+        if(temp >= 100){
+            System.out.println("vapor");
+        }else if(temp > 0){
+            System.out.println("water");
+        }else {
+            System.out.println("ice");
+        }
+
+
+    }
+}
