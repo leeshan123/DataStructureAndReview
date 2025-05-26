138b2f6ed8e8bcfad4ca9420f6ae086153cf96b9
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-05-26 12:43:26 +0900

    Title: 문자 출력하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250526/문자 출력하기/print-text.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        for(int i=0; i<8; i++){
+            System.out.print(input);
+        }
+
+
+
+    }
+}
