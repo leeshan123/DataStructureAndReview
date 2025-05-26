105de92e5d95d6bcb1b77f1ba9e6e30c7c74c40d
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-05-26 12:45:42 +0900

    Title: 문자열 출력하기, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250526/문자열 출력하기/print-string.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<input; i++){
+            System.out.println("LeebrosCode");
+        }
+
+    }
+}
