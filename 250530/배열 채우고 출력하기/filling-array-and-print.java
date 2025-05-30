495503a2bec2512dacf7b16496ea9890988c2148
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-05-30 09:46:56 +0900

    Title: 배열 채우고 출력하기, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

--- 250530/배열 채우고 출력하기/filling-array-and-print.java
+++ 250530/배열 채우고 출력하기/filling-array-and-print.java
@@ -1,20 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
-        char[] arr = new char[10];
-        arr[0] = 'L';
-        arr[1] = 'E';
-        arr[2] = 'B';
-        arr[3] = 'R';
-        arr[4] = 'O';
-        arr[5] = 'S';
-        arr[6] = 'C';
-        arr[7] = 'O';
-        arr[8] = 'D';
-        arr[9] = 'E';
+    public static void main(String[] args) throws IOException {
+        // 표준 입력(System.in)을 BufferedReader로 감싸기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        String[] tokens = input.split(" ");
+
+
 
         for(int i=9; i>=0; i--){
-            System.out.print(arr[i]);
+            System.out.print(tokens[i]);
         }
 
     }
