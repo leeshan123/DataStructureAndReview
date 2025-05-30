c87008e5cff2fabf9d48a009098208c40da223aa
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-05-30 09:43:30 +0900

    Title: 배열 채우고 출력하기, Time: 94ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250530/배열 채우고 출력하기/filling-array-and-print.java
+++ 250530/배열 채우고 출력하기/filling-array-and-print.java
@@ -3,11 +3,19 @@ public class Main {
         // Please write your code here.
         char[] arr = new char[10];
         arr[0] = 'L';
-        arr[0] = 'L';
-        arr[0] = 'L';
-        arr[0] = 'L';
-        arr[0] = 'L';
+        arr[1] = 'E';
+        arr[2] = 'B';
+        arr[3] = 'R';
+        arr[4] = 'O';
+        arr[5] = 'S';
+        arr[6] = 'C';
+        arr[7] = 'O';
+        arr[8] = 'D';
+        arr[9] = 'E';
 
+        for(int i=9; i>=0; i--){
+            System.out.print(arr[i]);
+        }
 
     }
 }
