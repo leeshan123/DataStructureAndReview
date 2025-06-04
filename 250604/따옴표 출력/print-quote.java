421e9a9525262f04701bc19360815e3e6119e54f
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2025-06-04 12:50:20 +0900

    Title: 따옴표 출력, Time: 100ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250604/따옴표 출력/print-quote.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = "He says \"It's a really simple sentence\".";
+
+        System.out.println(s);
+
+
+    }
+}
